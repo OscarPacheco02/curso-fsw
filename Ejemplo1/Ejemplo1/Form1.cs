717e2f5596e71b6410be099caf3678d21ad51e3b
OscarPacheco02 <oscar_pacheco02@hotmail.com>
2016-10-29 11:34:46 -0500

    Agregando Hola Mundo ///_u

--- Ejemplo1/Ejemplo1/Form1.cs
+++ Ejemplo1/Ejemplo1/Form1.cs
@@ -16,5 +16,10 @@ namespace Ejemplo1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello FIME");
+        }
     }
 }
